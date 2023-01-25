921b4771c27e479df2c87024e1b7f077941dc455
Joseph Gruber <joseph.gruber@gmail.com>
2023-01-24 18:10:33 -0700

    Remove deprecated feature

--- tf/s3.tf
+++ tf/s3.tf
@@ -7,11 +7,6 @@ resource "aws_s3_bucket" "main" { #tfsec:ignore:aws-s3-enable-bucket-logging tfs
     }
   )
   force_destroy = false
-
-  website {
-    index_document = "index.html"
-    error_document = "index.html"
-  }
 }
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "main" { #tfsec:ignore:aws-s3-encryption-customer-key
@@ -33,4 +28,16 @@ resource "aws_s3_bucket_public_access_block" "main" {
   block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_website_configuration" "main" {
+  bucket = aws_s3_bucket.main.bucket
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
 }
